f67fabcac47af720c6f148fe2e46e4dfcc615245
EPERSON <epersoncmxf@gmail.com>
2020-10-21 12:12:17 -0300

    Fixed animation bug when path constraint margin is different of zero

--- modules/planet.py
+++ modules/planet.py
@@ -7,8 +7,18 @@ class Planet(Object):
         super().__init__(obj)
     
     def animate_through_ellipse(self, ellipse, frame_step):
+
+        #get current frame
         current_frame = bpy.context.scene.frame_current
+
+        #set frame to current - 1 to avoid bugs on the first frame of the animation
+        bpy.context.scene.frame_set(current_frame - 1)
+
+        #for through all area points generating the animation
         for i in range(len(ellipse.areaPoints)):
             self.object.keyframe_insert(data_path='constraints["Follow Path"].offset', frame=current_frame + i * frame_step)
             self.object.constraints["Follow Path"].offset = ellipse.areaPoints[i]
+        
+        #set frame to the previous one
+        bpy.context.scene.frame_set(current_frame)
 
